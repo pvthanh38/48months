2181c5cf48eb7d13bd58124ca3f2561995b94350
KPI KPI <kpi@mkpi.c.tensile-topic-250504.internal>
2019-08-22 10:46:25 +0000

    connect database

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,9 @@ define('DB_NAME', 'wp_48months');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+
+
+define('DB_PASSWORD', '6+;dMEtJ{M8b');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
